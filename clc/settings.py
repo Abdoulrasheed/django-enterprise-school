40b53b3cbedf4383cbf71d57bd225aa75d5fb2be
abdoulrasheed <abdulrasheedibraheem47@yahoo.com>
2019-05-04 10:41:29 +0100

    Updated settings.py to reflect to the latest version of whitenouse

--- clc/settings.py
+++ clc/settings.py
@@ -136,4 +136,4 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
